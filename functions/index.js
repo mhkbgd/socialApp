fb77b9fb72fbcdde7873a7081f62f2c9e1be7c0c
makhan <makhan@ttu.ee>
2020-05-22 23:33:21 +0300

    signup and get user api!

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,25 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 admin.initializeApp();
+const firebaseConfig = {
+    apiKey: "AIzaSyBUDokpr38K0zmV-La-zIkVxrizhs4qi98",
+    authDomain: "socialapp-fceaa.firebaseapp.com",
+    databaseURL: "https://socialapp-fceaa.firebaseio.com",
+    projectId: "socialapp-fceaa",
+    storageBucket: "socialapp-fceaa.appspot.com",
+    messagingSenderId: "97180486813",
+    appId: "1:97180486813:web:64bbd9a1512ca9efd09874",
+    measurementId: "G-MX9V7K817H"
+  };
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
 
-const express = require('express');
-const app = express();
-
+const db = admin.firestore();
 
 app.get('/screams', (req, res)=>{
 
-    admin
-    .firestore()
+    db
     .collection('screams')
     .orderBy('createdAt' , 'desc')
     .get()
@@ -32,10 +42,10 @@ app.post('/scream', (req, res)=>{
     const newScrem = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db
     .collection('screams')
     .add(newScrem)
     .then(doc =>{
@@ -48,5 +58,32 @@ app.post('/scream', (req, res)=>{
 
 })
 
+//sign up 
 
+app.post('/signup', (req, res) =>{
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    db.doc(`/users/${newUser.handle}`).get()
+    .then(doc =>{
+        if(doc.exists){
+            return res.status(400).json({handle: 'this handle is already taken'});
+        }else{
+            return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        }
+    })
+    .then(data => {
+        return data.user.getIdTokern();
+    })
+    .then(token =>{
+        return res.status(201).json({ token });
+    })
+    .catch(err =>{
+        return res.status(500).json({error: err.code});
+    })
+})
    exports.api = functions.https.onRequest(app);
